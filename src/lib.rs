06846a9067271ed3549e608aa47c6ff6f7581219
Romain Ruetschi <romain.ruetschi@gmail.com>
2015-04-18 18:49:21 +0200

    Skeleton of the compiler plugin.

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+
+#![crate_type="dylib"]
+#![feature(plugin_registrar, rustc_private)]
+
+extern crate syntax;
+extern crate rustc;
+
+use syntax::ext::base::{ItemModifier, ExtCtxt, Modifier};
+use syntax::codemap::Span;
+use syntax::ptr::P;
+use syntax::ast::{Item, MetaItem};
+use syntax::parse::token::intern;
+use rustc::plugin::Registry;
+
+struct HelloWorldExpander;
+
+impl HelloWorldExpander {
+    fn new() -> HelloWorldExpander {
+        HelloWorldExpander
+    }
+}
+
+impl ItemModifier for HelloWorldExpander {
+    fn expand(&self, ecx: &mut ExtCtxt, span: Span, meta_item: &MetaItem, item: P<Item>) -> P<Item> {
+        item
+    }
+}
+
+#[plugin_registrar]
+pub fn plugin_registrar(reg: &mut Registry) {
+    let expander = Modifier(Box::new(HelloWorldExpander::new()));
+    reg.register_syntax_extension(intern("hello_world"), expander);
+}
+
+
